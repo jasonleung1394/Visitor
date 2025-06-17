d4c444c3b55a4472e9cbe948ef6c38c7cc70f9b9
json <jasonleung1394@gmail.com>
2025-06-17 10:31:53 +1000

    add static

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.json({ limit: '2mb' }));
 const renderRoute = require('./routes/renderRoute');
 app.use('/render', renderRoute);
 
+const renderFrontEnd = require('./routes/renderFrontEnd');
+app.use('/render_fe', renderFrontEnd);
+
+
 // Default
 app.get('/', (req, res) => {
   res.send('Microservice is up');
---
+++ routes/renderFrontEnd.js
@@ -0,0 +1,10 @@
+// in routes/renderFrontEnd.js
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
